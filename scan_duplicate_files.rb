32dbb54f8f5f784a79bddaa04d3d13446991d87b
Ibnu Toriq <ibnutoriq@gmail.com>
2018-09-07 23:42:39 +0700

    Add scan duplicate files function

---
+++ scan_duplicate_files.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/ruby
+
+require 'find'
+require 'digest/md5'
+require 'pry'
+
+def scan_duplicate_files(path)
+  sizes = {}
+  md5s = {}
+
+  Find.find(path) do |file|
+    (sizes[File.size(file)] ||= []) << file if File.file?(file)
+  end
+
+  sizes.each do |size, files|
+    next unless files.size > 1
+
+    files.each do |f|
+      digest = Digest::MD5.hexdigest(File.read(f))
+
+      (md5s[digest] ||= []) << f
+    end
+  end
+
+  paths = md5s
+    .map {|k, v| v}
+    .sort_by {|v| -v.size }
+    .first
+    
+  puts "#{File.read(paths.first)} #{paths.length}"
+end
+
+scan_duplicate_files(ARGV[0])
