b8571198e5c1f54645a5bb9bf4dff6c80a0c95f7
Ibnu Toriq <ibnutoriq@gmail.com>
2018-09-21 07:28:48 +0700

    Make code more readable and little bit faster

--- scan_duplicate_files.rb
+++ scan_duplicate_files.rb
@@ -8,12 +8,12 @@ def scan_duplicate_files(path)
   sizes = {}
   md5s = {}
 
-  Find.find(path) do |file|
+  Dir.glob("#{path}/**/*") do |file|
     (sizes[File.size(file)] ||= []) << file if File.file?(file)
   end
 
   sizes.each do |_, files|
-    next unless files.size > 1
+    next if files.size == 1
 
     files.each do |f|
       digest = Digest::MD5.hexdigest(File.read(f))
@@ -22,7 +22,7 @@ def scan_duplicate_files(path)
     end
   end
 
-  paths = md5s.map { |_, v| v }.min_by { |v| -v.size }
+  paths = md5s.map { |_, v| v }.max_by { |v| v.size }
 
   puts "#{File.read(paths.first)} #{paths.length}"
 end
